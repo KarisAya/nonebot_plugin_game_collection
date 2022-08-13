17f5e97bb49273b38177425bb410bf9fd821a1ef
KarisAya <1048827424@qq.com>
2022-08-13 22:11:52 +0800

    扑克对战初始点数

--- data_source.py
+++ data_source.py
@@ -245,7 +245,7 @@ class GameManager:
                             "——————————————\n"
                             f'玩家：{self._current_player[event.group_id]["player2"]}\n'
                             "状态：\n"
-                            f'HP 20  SP 5  DEF 5\n'
+                            f'HP 20  SP 5  DEF 0\n'
                             "——————————————\n"
                             f'当前回合：{self._current_player[event.group_id]["player1"]}\n'
                             "手牌：\n"
@@ -433,7 +433,7 @@ class GameManager:
                         "hand":[],
                         "HP":20,
                         "ATK":0,
-                        "DEF":5,
+                        "DEF":0,
                         "SP":5
                         }
                     } 

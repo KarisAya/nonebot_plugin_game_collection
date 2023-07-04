205ebdd8ff1b3ebca04a339bfaa13b9d29eb13e1
KarisAya <1048827424@qq.com>
2023-07-04 14:22:15 +0800

    修复 Nan

--- data.py
+++ data.py
@@ -182,7 +182,7 @@ class DataBase(BaseModel):
                 # 股票数检查
                 for company_id,count in stocks.items():
                     stock_check[company_id] += count
-                group_account.value == 0 if group_account.value == float("nan") else group_account.value
+                group_account.value == 0.0 if group_account.value != group_account.value else group_account.value
             # 金币总数
             log += f"{user.nickname} 金币总数异常。记录值：{user.gold} 实测值：{gold} 数据已修正。\n" if user.gold != gold else ""
             user.gold = gold
@@ -222,9 +222,9 @@ class DataBase(BaseModel):
                     "数据已修正。\n"
                     ) if company.stock + stock_check[group_id] != company.issuance else ""
                 company.stock = stock
-                company.gold = 0.0 if company.gold == float("nan") else company.gold
-                company.float_gold = 0.0 if company.float_gold == float("nan") else company.float_gold
-                company.group_gold = 0.0 if company.group_gold == float("nan") else company.group_gold
+                company.gold = 0.0 if company.gold != company.gold else company.gold
+                company.float_gold = 0.0 if company.float_gold != company.float_gold else company.float_gold
+                company.group_gold = 0.0 if company.group_gold != company.group_gold else company.group_gold
         self.save()
         return log[:-1] if log else "数据一切正常！"
 

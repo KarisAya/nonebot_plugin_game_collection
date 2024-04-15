c44e1bb89b6d4b55c4895bf73936f753f5989c77
KarisAya <1048827424@qq.com>
2024-04-15 20:26:03 +0800

    PluginMetadata

--- nonebot_plugin_game_collection/__init__.py
+++ nonebot_plugin_game_collection/__init__.py
@@ -1,6 +1,16 @@
 from clovers.core.plugin import PluginLoader
 from nonebot_plugin_clovers import adapter
 from nonebot.log import logger
+from nonebot.plugin import PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name="小游戏合集",
+    description="各种群内小游戏",
+    usage="金币签到",
+    type="application",
+    homepage="https://github.com/KarisAya/nonebot_plugin_game_collection",
+)
+
 
 plugin = PluginLoader.load("clovers_leafgame")
 if plugin is None:

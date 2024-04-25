6b226f568af7ba97156dfd1583517804c5bb4011
KarisAya <1048827424@qq.com>
2024-04-26 00:51:42 +0800

    修改clovers配置

--- nonebot_plugin_game_collection/__init__.py
+++ nonebot_plugin_game_collection/__init__.py
@@ -1,16 +1,23 @@
 from clovers.core.plugin import PluginLoader
-from nonebot_plugin_clovers import clovers
+from clovers.core.config import config
+
+supported_adapters = {"nonebot.adapters.onebot.v11"}
+config.setdefault("nonebot_plugin_clovers", {}).setdefault("using_adapters", set()).update(supported_adapters)
+
 from nonebot.log import logger
 from nonebot.plugin import PluginMetadata
 
+supported_adapters = {"nonebot.adapters.onebot.v11"}
 __plugin_meta__ = PluginMetadata(
     name="小游戏合集",
     description="各种群内小游戏",
     usage="金币签到",
     type="application",
     homepage="https://github.com/KarisAya/nonebot_plugin_game_collection",
-    supported_adapters={"nonebot.adapters.onebot.v11"},
+    supported_adapters=supported_adapters,
 )
+
+from nonebot_plugin_clovers import clovers
 
 
 def load_plugin(name: str):

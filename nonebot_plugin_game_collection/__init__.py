ad8d3d57f4cd2c64c6136bd95b77dc81b3a90aa9
KarisAya <1048827424@qq.com>
2024-04-27 16:37:11 +0800

    clovers (0.1.7)

--- nonebot_plugin_game_collection/__init__.py
+++ nonebot_plugin_game_collection/__init__.py
@@ -1,6 +1,4 @@
-from nonebot.log import logger
 from nonebot.plugin import PluginMetadata
-from clovers.core.plugin import PluginLoader
 from nonebot_plugin_clovers import clovers
 
 __plugin_meta__ = PluginMetadata(
@@ -12,17 +10,5 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters={"nonebot.adapters.onebot.v11"},
 )
 
-
-def load_plugin(name: str):
-    plugin = PluginLoader.load(name)
-    if plugin is None:
-        logger.error(f"未找到{name}")
-    elif plugin not in clovers.plugins:
-        clovers.plugins.append(plugin)
-        logger.success(f"{name}加载成功")
-    else:
-        logger.success(f"{name}已存在")
-
-
-load_plugin("clovers_apscheduler")
-load_plugin("clovers_leafgame")
+clovers.load_plugin("clovers_apscheduler")
+clovers.load_plugin("clovers_leafgame")

6dc5395b30b07ee6322af7b2e32df9d107633710
KarisAya <karisaya@foxmail.com>
2025-07-11 00:30:18 +0800

    4.0

--- nonebot_plugin_game_collection/__init__.py
+++ nonebot_plugin_game_collection/__init__.py
@@ -1,12 +1,6 @@
-from nonebot import on_message, get_driver
-from nonebot.matcher import Matcher
+from nonebot import require
 from nonebot.plugin import PluginMetadata
-from nonebot.adapters.onebot.v11 import Bot, MessageEvent
-from clovers import Leaf
-from nonebot_plugin_clovers import extract_command
-from nonebot_plugin_clovers.adapters.onebot.v11 import __adapter__ as onebot_v11
-from clovers_apscheduler import __plugin__ as apscheduler
-from clovers_leafgame import __plugin__ as leafgame
+
 
 __plugin_meta__ = PluginMetadata(
     name="小游戏合集",
@@ -17,29 +11,5 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters={"nonebot.adapters.onebot.v11"},
 )
 
-leaf = Leaf(onebot_v11)
-
-leaf.plugins.append(apscheduler)
-leaf.plugins.append(leafgame)
-
-
-driver = get_driver()
-
-Bot_NICKNAME = Bot_NICKNAMEs[0] if (Bot_NICKNAMEs := list(driver.config.nickname)) else "bot"
-
-
-@leaf.adapter.property_method("Bot_Nickname")
-async def _():
-    return Bot_NICKNAME
-
-
-driver.on_startup(leaf.startup)
-driver.on_shutdown(leaf.shutdown)
-
-main = on_message(priority=20, block=False)
-
-
-@main.handle()
-async def _(bot: Bot, event: MessageEvent, matcher: Matcher):
-    if await leaf.response(extract_command(event.get_plaintext()), bot=bot, event=event):
-        matcher.stop_propagation()
+IMPORT_NAME = "clovers_sarof"
+require("nonebot_plugin_clovers").client.load_plugin(IMPORT_NAME)
